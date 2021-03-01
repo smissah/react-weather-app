a14dfdeb51bc259cfff373e07c06e60706cb5671
stephenPC <stephenmissah@outlook.com>
2021-03-01 15:18:26 +0000

    added catch to fetch and ? to most react conditional renders

--- src/App.js
+++ src/App.js
@@ -45,12 +45,15 @@ const App = () => {
   const search = (evt) => {
     if (evt.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-        .then((res) => res.json())
+        .then((res) => res.json()) //.then is the suceess case. res is not the data - needs to be converted. this json returns a promise
         .then((result) => {
+          // this is the actual data
           setWeather(result);
           setQuery("");
           console.log(result);
-        });
+          return result;
+        })
+        .catch((err) => console.error(err));
     }
   };
 
@@ -102,13 +105,13 @@ const App = () => {
           <section>
             <div className="output">
               <div className="output__location">
-                {weather.name}, {weather.sys.country || ""}
+                {weather.name}, {weather.sys?.country || ""}
               </div>
               <div className="output__date">{dateBuilder(new Date())}</div>
             </div>
             <div className="weather">
               <div className="weather__temp">
-                {Math.round(weather.main.temp)}°c
+                {Math.round(weather.main?.temp)}°c
               </div>
               <div className="weather__weather">
                 <div className="weather__text">{weather.weather[0].main}</div>{" "}
